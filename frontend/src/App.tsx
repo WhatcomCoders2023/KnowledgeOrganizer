d8629549eb007b7ba2953924b490d8c62589806c
hanselguzman-soto <soto26938@gmail.com>
2023-04-03 15:13:51 -0700

    setup app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MediaDataTable from './components/Table';
+
+function App() {
+  return (
+    <div className="App">
+      <MediaDataTable />
+    </div>
+  );
+}
+
+export default App;
